e5bd672652bdd499cf596e46c87d9d73790fbe0a
Martin Wangen-Eriksen <martin.wangen-eriksen@bredvid.no>
2025-01-14 19:27:59 +0100

    add appshell

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,40 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import "@mantine/core/styles.css";
 import AdminPage from "./pages/AdminPage/AdminPage";
 import CurrentChallengePage from "./pages/CurrentChallengePage/CurrentChallengePage";
 import { RedirectPage } from "./pages/RedirectPage/RedirectPage";
 import { StartPage } from "./pages/StartPage/StartPage";
+import { AppShell, Flex, Tabs } from "@mantine/core";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="*" element={<StartPage />} />
-        <Route path="/redirect" element={<RedirectPage />} />
-        <Route path="/admin" element={<AdminPage />} />
-        <Route path="/currentchallenge" element={<CurrentChallengePage />} />
-      </Routes>
-    </Router>
+    <AppShell>
+      <AppShell.Header>
+        <Flex justify={"center"}>
+          <Tabs>
+            <Tabs.List>
+              <Tabs.Tab value="LOL">Lol</Tabs.Tab>
+              <Tabs.Tab value="LOL1">Lol1</Tabs.Tab>
+              <Tabs.Tab value="LOL2">Lol2</Tabs.Tab>
+            </Tabs.List>
+          </Tabs>
+        </Flex>
+      </AppShell.Header>
+      <AppShell.Main>
+        <Router>
+          <Routes>
+            <Route path="*" element={<StartPage />} />
+            <Route path="/redirect" element={<RedirectPage />} />
+            <Route path="/admin" element={<AdminPage />} />
+            <Route
+              path="/currentchallenge"
+              element={<CurrentChallengePage />}
+            />
+          </Routes>
+        </Router>
+      </AppShell.Main>
+    </AppShell>
   );
 }
 
